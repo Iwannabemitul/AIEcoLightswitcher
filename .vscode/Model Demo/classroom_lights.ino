063c0931dbfe01702c6612e7a73c7b22e8c623c6
Iwannabemitul <mitul.ar.2008@gmail.com>
2026-07-12 14:57:20 +0530

    Create classroom_lights.ino

---
+++ .vscode/Model Demo/classroom_lights.ino
@@ -0,0 +1,20 @@
+// Define the pin where your classroom LEDs are connected
+const int ledPin = 13; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW); // Start with lights off
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH);
+    } else if (command == '0') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
